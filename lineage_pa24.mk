59fce7805d5a801fc815a52d70a39e850ec3c0f9
Haruroid <Haruroid@users.noreply.github.com>
2019-08-23 22:50:17 +0900

    pa24: add AndroidProducts.mk

--- lineage_pa24.mk
+++ lineage_pa24.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/sharp/pa24/pa24.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_mini_phone.mk)
 
 PRODUCT_NAME := lineage_pa24
 PRODUCT_DEVICE := pa24
